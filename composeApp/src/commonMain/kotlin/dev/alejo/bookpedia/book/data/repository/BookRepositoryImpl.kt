142b9161c905c8e2665dbf35713be129c60ed2e8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:38:51 -0500

    Feat: Implement BookRepositoryImpl

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/repository/BookRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package dev.alejo.bookpedia.book.data.repository
+
+import dev.alejo.bookpedia.book.data.mappers.toBook
+import dev.alejo.bookpedia.book.data.network.RemoteBookDataSource
+import dev.alejo.bookpedia.book.domain.model.Book
+import dev.alejo.bookpedia.book.domain.repository.BookRepository
+import dev.alejo.bookpedia.core.domain.DataError
+import dev.alejo.bookpedia.core.domain.Result
+import dev.alejo.bookpedia.core.domain.map
+
+class BookRepositoryImpl(
+    private val remoteBookDataSource: RemoteBookDataSource
+) : BookRepository {
+    override suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote> {
+        return remoteBookDataSource
+            .searchBooks(query = query)
+            .map { response ->
+                response.results.map { it.toBook() }
+            }
+    }
+
+}
