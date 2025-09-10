fb52ffac57f72bd40a9c65c0d9dd854918bba4b7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:35:04 -0500

    Feat: Integrate BookListScreenRoot into App

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/App.kt
@@ -1,49 +1,33 @@
 package dev.alejo.bookpedia
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import dev.alejo.bookpedia.book.data.network.KtorRemoteBookDataSource
+import dev.alejo.bookpedia.book.data.repository.BookRepositoryImpl
+import dev.alejo.bookpedia.book.presentation.book_list.BookListScreenRoot
+import dev.alejo.bookpedia.book.presentation.book_list.BookListViewModel
+import dev.alejo.bookpedia.core.data.HttpClientFactory
+import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import cmp_bookpedia.composeapp.generated.resources.Res
-import cmp_bookpedia.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+fun App(engine: HttpClientEngine) {
+    BookListScreenRoot(
+        viewModel = remember {
+            BookListViewModel(
+                bookRepository = BookRepositoryImpl(
+                    remoteBookDataSource = KtorRemoteBookDataSource(
+                        httpClient = HttpClientFactory.create(
+                            engine = engine
+                        )
+                    )
+                )
+
+            )
+        },
+        onBookCLick = {
+
         }
-    }
+    )
 }
