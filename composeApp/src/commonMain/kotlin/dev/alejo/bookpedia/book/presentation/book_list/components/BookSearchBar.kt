b303afedfbe91adec0099a28c3d0b2146d0335ca
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:39:00 -0500

    Feat: Add BookSearchBar component

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -0,0 +1,92 @@
+package dev.alejo.bookpedia.book.presentation.book_list.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.clear_search
+import cmp_bookpedia.composeapp.generated.resources.search_placeholder
+import dev.alejo.bookpedia.core.presentation.DarkBlue
+import dev.alejo.bookpedia.core.presentation.DesertWhite
+import dev.alejo.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun BookSearchBar(
+    searchQuery: String,
+    onSearchQueryChange: (String) -> Unit,
+    onImeSearch: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            shape = RoundedCornerShape(100),
+            singleLine = true,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = SandYellow,
+                cursorColor = DarkBlue
+            ),
+            placeholder = {
+                Text(text = stringResource(Res.string.search_placeholder))
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                )
+            },
+            trailingIcon = {
+                AnimatedVisibility(visible = searchQuery.isNotBlank()) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(Res.string.clear_search),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                }
+            },
+            keyboardActions = KeyboardActions(
+                onSearch = { onImeSearch() }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            modifier = modifier
+                .background(shape = RoundedCornerShape(100), color = DesertWhite)
+                .minimumInteractiveComponentSize(),
+        )
+    }
+}
