ab40eb93ffd435ca872d3d5a2fc92f438ba07cf3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:40:05 -0500

    Feat: Add KtorRemoteBookDataSource to search books

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -0,0 +1,29 @@
+package dev.alejo.bookpedia.book.data.network
+
+import dev.alejo.bookpedia.book.data.dto.SearchedResponseDto
+import dev.alejo.bookpedia.core.data.safeCall
+import dev.alejo.bookpedia.core.domain.DataError
+import dev.alejo.bookpedia.core.domain.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+private const val BASE_URL= "https://openlibrary.org"
+
+class KtorRemoteBookDataSource(
+    private val httpClient: HttpClient
+) : RemoteBookDataSource {
+    override suspend fun searchBooks(
+        query: String,
+        resultLimit: Int?
+    ): Result<SearchedResponseDto, DataError.Remote> = safeCall {
+        httpClient.get(
+            urlString = "$BASE_URL/search.json"
+        ) {
+            parameter("q", query)
+            parameter("limit", resultLimit)
+            parameter("parameter", "eng")
+            parameter("fields", "key,title,cover_i,author_name,first_publish_year,ratings_average,ratings_count,number_of_pages_median,edition_count")
+        }
+    }
+}
