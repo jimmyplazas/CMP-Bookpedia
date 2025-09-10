d50a443a9824dbb830a6245c0ff83cb3a63e3ebc
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:40:29 -0500

    Feat: Add previews for BookSearchBar, BookListItem, and BookListScreen

---
+++ composeApp/src/androidMain/kotlin/dev/alejo/bookpedia/Previews.kt
@@ -0,0 +1,59 @@
+package dev.alejo.bookpedia
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.alejo.bookpedia.book.domain.model.Book
+import dev.alejo.bookpedia.book.presentation.book_list.BookListScreen
+import dev.alejo.bookpedia.book.presentation.book_list.BookListState
+import dev.alejo.bookpedia.book.presentation.book_list.components.BookListItem
+import dev.alejo.bookpedia.book.presentation.book_list.components.BookSearchBar
+import dev.alejo.bookpedia.book.presentation.book_list.dummyBooks
+
+@Preview
+@Composable
+private fun BookSearchBarPreview() {
+    MaterialTheme {
+        BookSearchBar(
+            searchQuery = "Kotlin",
+            onSearchQueryChange = {},
+            onImeSearch = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun BookItemPreview() {
+    BookListItem(
+        book = Book(
+            id = "1",
+            title = "Hey",
+            imageUrl = "asdasdasd",
+            authors = listOf("asdasd", "sadfsdf"),
+            description = "asdasdasdasdasdas",
+            languages = listOf("es", "en"),
+            firstPublicYear = "asdasd",
+            averageRating = 3.5,
+            ratingCount = 3,
+            numPages = 200,
+            numEditions = 3
+        ),
+        onClick = {}
+    )
+}
+
+@Preview
+@Composable
+private fun BookScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = dummyBooks
+        ),
+        onAction = {},
+        modifier = Modifier.fillMaxWidth()
+    )
+}
