4b1e16b68d5489b118f18239e891b6dea2e76f49
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-10 16:35:59 -0500

    Refactor: Remove platform-specific greeting code

--- composeApp/src/androidMain/kotlin/dev/alejo/bookpedia/Platform.android.kt
+++
@@ -1,9 +0,0 @@
-package dev.alejo.bookpedia
-
-import android.os.Build
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.alejo.bookpedia
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
