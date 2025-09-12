98ab4ceb516ffb60bed80e6f2625b63dcacf75d4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-12 13:13:03 -0500

    Feat: Integrate Koin for ViewModel injection in App.kt

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/App.kt
@@ -1,31 +1,17 @@
 package dev.alejo.bookpedia
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import dev.alejo.bookpedia.book.data.network.KtorRemoteBookDataSource
-import dev.alejo.bookpedia.book.data.repository.BookRepositoryImpl
 import dev.alejo.bookpedia.book.presentation.book_list.BookListScreenRoot
 import dev.alejo.bookpedia.book.presentation.book_list.BookListViewModel
-import dev.alejo.bookpedia.core.data.HttpClientFactory
-import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     BookListScreenRoot(
-        viewModel = remember {
-            BookListViewModel(
-                bookRepository = BookRepositoryImpl(
-                    remoteBookDataSource = KtorRemoteBookDataSource(
-                        httpClient = HttpClientFactory.create(
-                            engine = engine
-                        )
-                    )
-                )
-
-            )
-        },
+        viewModel = viewModel,
         onBookCLick = {
 
         }
