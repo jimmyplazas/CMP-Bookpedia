a208d8956fce4969db7997060151a02787bb7e4b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-12 13:13:09 -0500

    Refactor: Remove dummy books from BookListState

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,25 +5,9 @@ import dev.alejo.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "",
-    val searchResults: List<Book> = dummyBooks,
+    val searchResults: List<Book> = emptyList(),
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
 )
-
-val dummyBooks = (1..100).map {
-    Book(
-        id = it.toString(),
-        title = "Hey",
-        imageUrl = "asdasdasd",
-        authors = listOf("asdasd", "sadfsdf"),
-        description = "asdasdasdasdasdas",
-        languages = listOf("es", "en"),
-        firstPublicYear = "asdasd",
-        averageRating = 3.5,
-        ratingCount = 3,
-        numPages = 200,
-        numEditions = 3
-    )
-}
