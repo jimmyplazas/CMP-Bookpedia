98c1ba039d878be49bccf620e78b7f5589bcf2ff
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-12 13:14:30 -0500

    Feat: Add Koin modules for dependency injection

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/di/Modules.kt
@@ -0,0 +1,23 @@
+package dev.alejo.bookpedia.di
+
+import dev.alejo.bookpedia.book.data.network.KtorRemoteBookDataSource
+import dev.alejo.bookpedia.book.data.network.RemoteBookDataSource
+import dev.alejo.bookpedia.book.data.repository.BookRepositoryImpl
+import dev.alejo.bookpedia.book.domain.repository.BookRepository
+import dev.alejo.bookpedia.book.presentation.book_list.BookListViewModel
+import dev.alejo.bookpedia.core.data.HttpClientFactory
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule = module {
+    single { HttpClientFactory.create(get()) }
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+    singleOf(::BookRepositoryImpl).bind<BookRepository>()
+
+    viewModelOf(::BookListViewModel)
+}
