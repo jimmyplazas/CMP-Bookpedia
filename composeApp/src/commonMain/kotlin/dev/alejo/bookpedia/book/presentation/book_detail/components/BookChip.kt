a0402def94360ef2679d25e116fc5becc5224b01
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-15 19:47:29 -0500

    Feat: Add BookChip composable with customizable size

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_detail/components/BookChip.kt
@@ -0,0 +1,42 @@
+package dev.alejo.bookpedia.book.presentation.book_detail.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import dev.alejo.bookpedia.book.presentation.book_detail.components.ChipSize.REGULAR
+import dev.alejo.bookpedia.book.presentation.book_detail.components.ChipSize.SMALL
+import dev.alejo.bookpedia.core.presentation.LightBlue
+
+enum class ChipSize {
+    SMALL, REGULAR
+}
+
+@Composable
+fun BookChip(
+    modifier: Modifier = Modifier,
+    size: ChipSize = ChipSize.REGULAR,
+    chipContent: @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .widthIn(
+                min = when (size) {
+                    SMALL -> 50.dp
+                    REGULAR -> 80.dp
+                }
+            )
+            .clip(RoundedCornerShape(16.dp))
+            .background(LightBlue)
+            .padding(vertical = 8.dp, horizontal = 12.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        chipContent()
+    }
+}
