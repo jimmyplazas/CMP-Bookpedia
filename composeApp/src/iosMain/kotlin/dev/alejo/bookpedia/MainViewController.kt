5446c384b0776d92ab8af9148fdfbc48d52465c1
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-15 19:46:49 -0500

    Feat: Add Koin initialization for iOS

--- composeApp/src/iosMain/kotlin/dev/alejo/bookpedia/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/dev/alejo/bookpedia/MainViewController.kt
@@ -1,6 +1,7 @@
 package dev.alejo.bookpedia
 
 import androidx.compose.ui.window.ComposeUIViewController
+import dev.alejo.bookpedia.app.App
 import dev.alejo.bookpedia.di.initKoin
 
 fun MainViewController() = ComposeUIViewController(
