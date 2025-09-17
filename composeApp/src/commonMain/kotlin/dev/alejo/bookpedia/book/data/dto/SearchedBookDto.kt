67d8d4dae9475cd1024cd669693e4e41262149b1
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 10:08:21 -0500

    Fix: Initialize languages as empty list in SearchedBookDto

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/dto/SearchedBookDto.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/dto/SearchedBookDto.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 data class SearchedBookDto(
     @SerialName("key") val id: String,
     @SerialName("title") val title: String,
-    @SerialName("language") val languages: List<String>? = null,
+    @SerialName("language") val languages: List<String> = emptyList(),
     @SerialName("cover_i") val coverAlternativeKey: Int? = null,
     @SerialName("author_key") val authorKeys: List<String>? = null,
     @SerialName("author_name") val authorNames: List<String>? = null,
