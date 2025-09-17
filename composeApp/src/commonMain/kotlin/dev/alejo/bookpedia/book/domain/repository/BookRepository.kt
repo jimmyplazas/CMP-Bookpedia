27c11a0a444ffa62a3fe17f48e6699745f516c3b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 15:56:57 -0500

    Feat: Add getBookDescription method to BookRepository

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/domain/repository/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/domain/repository/BookRepository.kt
@@ -6,4 +6,5 @@ import dev.alejo.bookpedia.core.domain.Result
 
 interface BookRepository {
     suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
+    suspend fun getBookDescription(bookWorkId: String): Result<String?, DataError>
 }
