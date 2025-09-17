161ba87c851ef5d402d2e30d750ec2007995c970
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 15:57:16 -0500

    Feat: Add function to get book description

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/network/RemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/data/network/RemoteBookDataSource.kt
@@ -1,5 +1,6 @@
 package dev.alejo.bookpedia.book.data.network
 
+import dev.alejo.bookpedia.book.data.dto.BookWorkDto
 import dev.alejo.bookpedia.book.data.dto.SearchedResponseDto
 import dev.alejo.bookpedia.core.domain.DataError
 import dev.alejo.bookpedia.core.domain.Result
@@ -9,4 +10,6 @@ interface RemoteBookDataSource {
         query: String,
         resultLimit: Int? = null
     ) : Result<SearchedResponseDto, DataError.Remote>
+
+    suspend fun getBookDescription(bookWorkId: String): Result<BookWorkDto, DataError.Remote>
 }
