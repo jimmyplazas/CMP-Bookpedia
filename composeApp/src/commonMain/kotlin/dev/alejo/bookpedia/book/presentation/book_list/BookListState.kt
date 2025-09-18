6afd63119aa8fa38ee9d38c6ca140ff68cb98783
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 19:10:58 -0500

    Refactor: Rename `favoriteBooks` to `favouriteBooks` in `BookListState`

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/BookListState.kt
@@ -6,7 +6,7 @@ import dev.alejo.bookpedia.core.presentation.UiText
 data class BookListState(
     val searchQuery: String = "Kotlin",
     val searchResults: List<Book> = emptyList(),
-    val favoriteBooks: List<Book> = emptyList(),
+    val favouriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null
