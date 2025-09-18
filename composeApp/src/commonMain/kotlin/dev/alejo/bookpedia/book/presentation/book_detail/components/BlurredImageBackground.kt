6eaab5168df30fcc085a3dceb68238c4f1b124ae
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 19:06:45 -0500

    Feat: Enhance book detail screen with loading animation and transparent card

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -19,7 +20,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -47,6 +47,7 @@ import cmp_bookpedia.composeapp.generated.resources.server_error
 import coil3.compose.rememberAsyncImagePainter
 import dev.alejo.bookpedia.core.presentation.DarkBlue
 import dev.alejo.bookpedia.core.presentation.DesertWhite
+import dev.alejo.bookpedia.core.presentation.PulseAnimation
 import dev.alejo.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -128,9 +129,6 @@ fun BlurredImageBackground(
                     .height(260.dp)
                     .aspectRatio(2 / 3f),
                 shape = RoundedCornerShape(12.dp),
-                colors = CardDefaults.elevatedCardColors(
-                    containerColor = Color.Transparent
-                ),
                 elevation = CardDefaults.elevatedCardElevation(
                     15.dp
                 )
@@ -139,7 +137,9 @@ fun BlurredImageBackground(
                     targetState = imageLoadResult
                 ) { result ->
                     when (result) {
-                        null -> CircularProgressIndicator()
+                        null -> Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            PulseAnimation(modifier = Modifier.size(60.dp))
+                        }
                         else -> {
                             Box(
                                 contentAlignment = Alignment.Center
