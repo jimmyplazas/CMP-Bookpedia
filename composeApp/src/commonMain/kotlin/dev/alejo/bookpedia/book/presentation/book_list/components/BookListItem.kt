7fd86c514579bf38a59966f1b06e0ad171911370
Jimmyale3102 <jimmyale3102@gmail.com>
2025-09-17 19:10:39 -0500

    Refactor: Enhance BookListItem with image loading animation and placeholder

--- composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package dev.alejo.bookpedia.book.presentation.book_list.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,7 +17,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -27,15 +28,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.book_error
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import dev.alejo.bookpedia.book.domain.model.Book
 import dev.alejo.bookpedia.core.presentation.LightBlue
+import dev.alejo.bookpedia.core.presentation.PulseAnimation
 import dev.alejo.bookpedia.core.presentation.SandYellow
 import org.jetbrains.compose.resources.painterResource
 import kotlin.math.round
@@ -82,17 +87,35 @@ fun BookListItem(
                         imageLoadResult = Result.failure(it.result.throwable)
                     }
                 )
+
+                val painterState by painter.state.collectAsStateWithLifecycle()
+                val transition by animateFloatAsState(
+                    targetValue = if (painterState is AsyncImagePainter.State.Success) {
+                        1f
+                    } else {
+                        0f
+                    },
+                    animationSpec = tween(500)
+                )
+
                 when (val result = imageLoadResult) {
-                    null -> CircularProgressIndicator()
+                    null -> PulseAnimation(modifier = Modifier.size(60.dp))
                     else -> {
                         Image(
                             painter = if (result.isSuccess) painter else painterResource(Res.drawable.book_error),
                             contentDescription = book.title,
                             contentScale = if (result.isSuccess) ContentScale.Crop else ContentScale.Fit,
-                            modifier = Modifier.aspectRatio(
-                                ratio = 0.65f,
-                                matchHeightConstraintsFirst = true
-                            )
+                            modifier = Modifier
+                                .aspectRatio(
+                                    ratio = 0.65f,
+                                    matchHeightConstraintsFirst = true
+                                )
+                                .graphicsLayer {
+                                    rotationX = (1f - transition) * 50f
+                                    val scale = 0.8f + (0.2f * transition)
+                                    scaleX = scale
+                                    scaleY = scale
+                                }
                         )
                     }
                 }
